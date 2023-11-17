b58f1949453e08a98f2f287fa4757c43737acae9
euanjohnston92-berlin <60107585+euanjohnston92-berlin@users.noreply.github.com>
2023-11-17 14:08:28 +0100

    adding env variable notation to run locally without impacting prod run. Changed stg model for property analytics

--- dbt/property_analytics/models/staging/stg_property_analytics.sql
+++ dbt/property_analytics/models/staging/stg_property_analytics.sql
@@ -1,6 +1,21 @@
 {{ config(materialized='table') }}
 
-select *, 
-       CURRENT_DATETIME() AS dbt_loaded_at_utc,
-       '{{ var("job_id") }}' AS dbt_job_id
-FROM {{ source('property_analytics', 'property_source_main') }}
+WITH ranked_data AS (
+  SELECT
+    parel.*,
+    CAST(inserted_at AS DATE) AS date_,
+    ROW_NUMBER() OVER (PARTITION BY CAST(inserted_at AS DATE), property_code ORDER BY inserted_at DESC) AS row_num
+FROM {{ source('property_analytics', 'property_source_main') }} parel
+  LEFT JOIN
+       {{ source('property_analytics', 'pagination_metadata') }} par  ON parel._dlt_parent_id = par._dlt_id
+  LEFT JOIN
+         {{ source('property_analytics', 'load_metadata') }} dl ON par._dlt_load_id = dl.load_id
+)
+SELECT
+  * Except(date_, row_num),
+  CURRENT_DATETIME() AS dbt_loaded_at_utc,
+  '{{ var("job_id") }}' AS dbt_job_id
+FROM
+  ranked_data
+WHERE
+  row_num = 1
