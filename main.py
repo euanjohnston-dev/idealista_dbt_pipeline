aa6d6ae68bb3a6c1fd60a4c9b6c2c2e600758513
euanjohnston92-berlin <60107585+euanjohnston92-berlin@users.noreply.github.com>
2023-11-21 09:24:55 +0100

    Ammended the main.py pipeline to include slack messaging

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import logging
 from dbt_run_pipeline import dbt_run
 import base64
+from slack_messaging import pipeline_success, pipeline_failure
+
 
 def dbt_pipeline(data, context):
     """Triggered by a Pub/Sub message."""
@@ -15,7 +17,9 @@ def dbt_pipeline(data, context):
 
         # Run the DBT command
         dbt_run()
+        pipeline_success()
 
         logging.info("DBT command executed successfully.")
     except Exception as e:
+        pipeline_failure(e)
         logging.error(f"Error executing DBT command: {e}")
