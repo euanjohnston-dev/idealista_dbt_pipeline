95915a7aed251981cfec51378b7d3e80a176bf20
euanjohnston92-berlin <60107585+euanjohnston92-berlin@users.noreply.github.com>
2023-11-22 11:09:38 +0100

    refactored dim_property_duplicates making it more efficient removing implicit duplicates

--- dbt/property_analytics/models/curated/dim_duplicates_to_check.sql
+++ dbt/property_analytics/models/curated/dim_duplicates_to_check.sql
@@ -42,31 +42,61 @@ Differentials AS (
 FilteredDifferentials AS (
   SELECT cpd.*
   FROM Differentials cpd
-  LEFT JOIN  {{ ref("stg_property_duplicates") }}  pdu
-  ON (cpd.first_duplicate = pdu.parent_property AND cpd.second_duplicate = pdu.child_property)
-      OR (cpd.first_duplicate = pdu.child_property AND cpd.second_duplicate = pdu.parent_property)
-  LEFT JOIN  {{ ref("stg_property_distinct") }} pdi 
-  ON (cpd.first_duplicate = pdi.parent_property AND cpd.second_duplicate = pdi.child_property)
-      OR (cpd.first_duplicate = pdi.child_property AND cpd.second_duplicate = pdi.parent_property)
+  LEFT JOIN {{ ref("stg_property_duplicates") }} pdu
+  ON (cpd.first_duplicate = CAST(pdu.parent_property AS STRING) AND cpd.second_duplicate = pdu.child_property)
+      OR (cpd.first_duplicate = CAST(pdu.child_property AS STRING) AND cpd.second_duplicate = pdu.parent_property)
+  LEFT JOIN {{ ref("stg_property_distinct") }} pdi 
+  ON (cpd.first_duplicate = CAST(pdi.parent_property AS STRING) AND cpd.second_duplicate = pdi.child_property)
+      OR (cpd.first_duplicate = CAST(pdi.child_property AS STRING) AND cpd.second_duplicate = pdi.parent_property)
   WHERE pdi.parent_property IS NULL and pdu.parent_property IS NULL
     AND cpd.distance < 1
     AND cpd.first_price = cpd.second_price
     AND cpd.first_agency != cpd.second_agency
+), 
+EstablishExistingRelationships AS (
+  SELECT
+    DISTINCT
+    fd.first_duplicate AS original_first_duplicate,
+    fd.second_duplicate AS original_second_duplicate,
+    CASE
+      WHEN dpd1.parent_property IS NOT NULL THEN dpd1.parent_property
+      WHEN dpd2.parent_property IS NOT NULL THEN dpd2.parent_property
+      WHEN dpd5.parent_property IS NOT NULL THEN dpd5.parent_property
+      WHEN dpd6.parent_property IS NOT NULL THEN dpd6.parent_property
+      ELSE fd.first_duplicate
+    END AS parent_duplicate_to_check,
+    CASE
+      WHEN dpd1.parent_property IS NOT NULL THEN fd.second_duplicate
+      WHEN dpd2.parent_property IS NOT NULL THEN fd.first_duplicate
+      WHEN dpd5.parent_property IS NOT NULL THEN fd.second_duplicate
+      WHEN dpd6.parent_property IS NOT NULL THEN fd.first_duplicate
+      ELSE fd.second_duplicate
+    END AS child_duplicate_to_check
+  FROM
+    FilteredDifferentials fd
+  LEFT JOIN {{ ref("stg_property_duplicates") }} dpd1
+  ON
+    fd.first_duplicate = CAST(dpd1.parent_property AS STRING)
+  LEFT JOIN {{ ref("stg_property_duplicates") }} dpd2
+  ON
+    fd.second_duplicate = CAST(dpd2.parent_property AS STRING)
+  LEFT JOIN {{ ref("stg_property_duplicates") }} dpd3
+  ON
+    fd.first_duplicate = CAST(dpd3.child_property AS STRING)
+  LEFT JOIN {{ ref("stg_property_duplicates") }} dpd4
+  ON
+    fd.second_duplicate = CAST(dpd4.child_property AS STRING)
+  LEFT JOIN {{ ref("stg_property_duplicates") }} dpd5
+  ON
+    dpd3.child_property = dpd5.child_property
+  LEFT JOIN {{ ref("stg_property_duplicates") }} dpd6
+  ON
+    dpd4.child_property = dpd6.child_property
+  WHERE NOT (dpd3.child_property IS NOT NULL AND dpd4.child_property IS NOT NULL)
 )
 
-SELECT DISTINCT
-  CASE
-    WHEN dpc.parent_property IS NOT NULL THEN dpc.parent_property
-    WHEN dpc2.parent_property IS NOT NULL THEN dpc2.parent_property
-  END AS parent_property,
-  CASE
-    WHEN dpc.parent_property IS NOT NULL THEN dpc.child_property
-    WHEN dpc2.parent_property IS NOT NULL THEN dpc2.child_property
-  END AS child_property
-FROM FilteredDifferentials dct
-LEFT JOIN {{ ref("stg_property_duplicates") }} dpc
-  ON dct.first_duplicate = dpc.parent_property OR dct.first_duplicate = dpc.child_property
-LEFT JOIN {{ ref("stg_property_duplicates") }} dpc2
-  ON dct.second_duplicate = dpc2.parent_property OR dct.second_duplicate = dpc2.child_property
-WHERE (dpc.parent_property IS NOT NULL AND dpc.child_property IS NOT NULL)
-   OR (dpc2.parent_property IS NOT NULL AND dpc2.child_property IS NOT NULL)
+SELECT parent_duplicate_to_check,
+       child_duplicate_to_check,
+       CURRENT_DATETIME() AS dbt_loaded_at_utc,
+       '{{ var("job_id") }}' AS dbt_job_id
+FROM EstablishExistingRelationships
