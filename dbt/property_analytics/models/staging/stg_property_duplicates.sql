c3a81877f3368ef9837f571c278a7a722e6fece2
euanjohnston92-berlin <60107585+euanjohnston92-berlin@users.noreply.github.com>
2023-11-29 14:54:51 +0100

    refactored dtg_property_duplicates

--- dbt/property_analytics/models/staging/stg_property_duplicates.sql
+++ dbt/property_analytics/models/staging/stg_property_duplicates.sql
@@ -10,15 +10,19 @@
 -- Select statement for the incremental model
 WITH table_base AS (
   SELECT
-    LOWER(TO_HEX(SHA256(CONCAT(CAST(property_code AS STRING), '_', CAST(duplicate_group_id AS STRING))))) AS ID,
     property_code,
     duplicate_group_id
   FROM {{ source('sheets_check', 'upload_duplicates') }}
+  union all 
+  SELECT
+    property_code,
+    duplicate_group_id
+  FROM {{ source('duplicate_processing', 'additional_duplicates') }}
 )
 
 SELECT
   Distinct 
-  ID,
+  LOWER(TO_HEX(SHA256(CONCAT(CAST(property_code AS STRING), '_', CAST(duplicate_group_id AS STRING))))) AS ID,
   CAST(property_code AS STRING) as property_code,
   CAST(duplicate_group_id AS STRING) as duplicate_group_id,
   CURRENT_DATETIME() AS dbt_loaded_at_utc,
