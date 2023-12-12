bd78e77ede8af2fd3627c7a5c6fca6ddc1951a10
euanjohnston92-berlin <60107585+euanjohnston92-berlin@users.noreply.github.com>
2023-12-12 11:18:08 +0100

    dropped slack function in favour of webhook

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
 import logging
 from dbt_run_pipeline import dbt_run
 import base64
-from slack_messaging import pipeline_success, pipeline_failure
+from dlt.common.runtime.slack import send_slack_message
 
+def notify_on_completion(hook):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            try:
+                result = func(*args, **kwargs)
+                message = f"Function {func.__name__} completed successfully"
+                send_slack_message(hook, message)
+                return result
+            except Exception as e:
+                message = f"Function {func.__name__} failed. Error: {str(e)}"
+                send_slack_message(hook, message)
+                raise
+        return wrapper
+    return decorator
 
-def dbt_pipeline(data, context):
+@notify_on_completion("https://hooks.slack.com/services/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")
+def dbt_pipeline(): #data, context
     """Triggered by a Pub/Sub message."""
-    logging.info(f"Function triggered by Pub/Sub event: {data}")
+   # logging.info(f"Function triggered by Pub/Sub event: {data}")
 
     try:
         # Decode the Pub/Sub message
-        message = base64.b64decode(data['data']).decode('utf-8')
-        logging.info(f"Received message data: {message}")
+    #    message = base64.b64decode(data['data']).decode('utf-8')
+     #   logging.info(f"Received message data: {message}")
 
         # Add your logic to process the Pub/Sub message if needed
 
         # Run the DBT command
-        
         dbt_run()
-        pipeline_success()
 
         logging.info("DBT command executed successfully.")
     except Exception as e:
-        pipeline_failure(e)
         logging.error(f"Error executing DBT command: {e}")
+
+dbt_pipeline()
